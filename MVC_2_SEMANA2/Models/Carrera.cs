c1b940ce38444450c7e8cf7b90d5ddfcee3cd5c2
M1LO225 <emilio.guerrero.argoti@udla.edu.ec>
2024-09-30 08:56:57 -0500

    primera clase

---
+++ MVC_2_SEMANA2/Models/Carrera.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_2_SEMANA2.Models
+{
+    public class Carrera
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        
+    }
+}
---
+++ MVC_2_SEMANA2/Models/EstudianteUDLA.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MVC_2_SEMANA2.Models
+{
+    public class EstudianteUDLA
+    {
+        [Key]
+        public string IdBanner { get; set; }
+        [AllowNull]
+        public string? Nombre { get; set; }
+        public Carrera Carrera { get; set; }
+
+    }
+
+   
+}
